8a12e31ceb158ae4beb2255a1b21604a39fe753c
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2024-04-16 06:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("count");
+function inc(){
+	let count = Number(para.innerText);
+	alert(count);
+	para.innerText = ++count;
+}
